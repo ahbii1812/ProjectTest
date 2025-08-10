8133b32e6c3c446b3ba326a21fb56208b79c0afc
ahbii1812 <ahbii.0630@gmail.com>
2025-08-11 01:11:14 +0800

    [Navigation] Update bottom nav bar

--- src/navigator/BottomTabNavigator.tsx
+++ src/navigator/BottomTabNavigator.tsx
@@ -2,14 +2,43 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../features/home/Home.screen';
 import { SCREEN_KEY } from '../constants/ScreenKey';
 import WishlistScreen from '../features/wishlist/Wishlist.screen';
+import { COLORS } from '../theme/theme';
+import { Image } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
-export default function BottomTabNavigator() {
+export default function MainTabs() {
   return (
     <Tab.Navigator
       initialRouteName={SCREEN_KEY.HOME}
-      screenOptions={{ headerShown: false }}
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: COLORS.darkTheme, // 👈 Custom background color
+          borderTopColor: 'transparent',
+        },
+        tabBarActiveTintColor: COLORS.theme, // Active icon/text color
+        tabBarInactiveTintColor: COLORS.white, // Inactive icon/text color
+        tabBarIcon: ({ color, size }) => {
+          if (route.name === SCREEN_KEY.HOME) {
+            return (
+              <Image
+                resizeMode="contain"
+                source={require(`../assets/home.png`)}
+                style={{ width: size, height: size, tintColor: color }}
+              />
+            );
+          }
+          return (
+            <Image
+              resizeMode="contain"
+              source={require(`../assets/bookmark.png`)}
+              style={{ width: size, height: size, tintColor: color }}
+            />
+          );
+        },
+      })}
     >
       <Tab.Screen name={SCREEN_KEY.HOME} component={HomeScreen} />
       <Tab.Screen name={SCREEN_KEY.WISHLIST} component={WishlistScreen} />
